be30719c3c4a8f3cac9752fc15db2ca07815889f
Mark Reynolds <mdr.photoshop@gmail.com>
2021-03-30 10:16:18 -0700

    Delete sever.js

--- sever.js
+++
@@ -1,7 +0,0 @@
-//The set of functions that I want to call in order
-export function getData(initialData) {
-    //gets the data
-    return new Promise(function (resolve, reject) {
-      resolve('Hello World!')
-    })
-  }
